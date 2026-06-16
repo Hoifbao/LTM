3f657d04c71590999ed807608ec9874f300688aa
John Doe <johndoe1@example.com>
2026-06-16 11:09:58 +0700

    Khởi tạo bộ khung dự án cho Server và Client

--- LabAdmin.Client/Form1.cs
+++ LabAdmin.Client/Form1.cs
@@ -16,5 +16,10 @@ namespace LabAdmin.Client
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
